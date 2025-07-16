c4bcbeb471088b38a0d20949d83ade1e375614b6
Pradeep Selakoti <pradeepselakoti@gmail.com>
2025-07-16 11:26:10 +0530

    02_variable

---
+++ 01_varaible.js
@@ -0,0 +1,15 @@
+const acc = 12222
+let acce = "k@gmail.com"
+var accp = "123"
+accity = "jaipur"
+
+acce = "ks@gm"
+accp = "222"
+accity ="jfjf"
+ // = 7
+
+
+//console.table([acce,accp,accity])
+
+
+console.log(typeof undefined)
---
+++ 02_dataTypes.js
@@ -0,0 +1,14 @@
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3+3); console.log("Hitesh")
+
+// number =>2 to power 53
+// bigInt
+// string => ""
+// boolena => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// 
