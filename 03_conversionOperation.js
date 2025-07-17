d31a0e722e7fe369ea3652435105662706e082a1
Pradeep Selakoti <pradeepselakoti@gmail.com>
2025-07-17 17:49:00 +0530

    conversion done

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = NaN
 
- console.log(typeof score)
+//  console.log(typeof score)
 // console.log(typeof(score))
 
 let valueInNumber = Number(score)
- console.log(typeof valueInNumber)
- console.log(valueInNumber)
+//  console.log(typeof valueInNumber)
+//  console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -28,5 +28,29 @@ let boolenIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ******************** Operations *************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "helllo"
+let str2 = " hitesh"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1 + "2")
+// console.log("1"+ 2 + 2)
+// console.log(1 +2+ "2")
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3=2+2
