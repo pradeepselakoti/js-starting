e692fafc35b628ce14f40d2c2ba2f7e0317f060b
Pradeep Selakoti <pradeepselakoti@gmail.com>
2025-07-18 11:57:17 +0530

    nums and maths

---
+++ 06_Strings.js
@@ -0,0 +1,37 @@
+const name ="pradeep"
+const repoCount = 50
+
+//console.log(name+ repoCount+" Value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gamename = new String('hello-praddeep-dep')
+
+// console.log(gamename[0]);
+
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length)
+// console.log(gamename.toUpperCase())
+
+// console.log(gamename.charAt(4))
+// console.log(gamename.indexOf('l'));
+
+const newString = gamename.substring(0,4) // in this we cannot start with 0 ; it will ignore it and start with 0
+//console.log(newString)
+
+const anothername = gamename.slice(-2,4)
+//console.log(anothername)
+
+const newStringOne = "  pradeep  "
+console.log(newStringOne);
+console.log(newStringOne.trim())
+
+const url = "https://pradeep.com/pradeep%20selakoti"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('pradeep'))
+
+console.log(gamename.split('-'))
+
---
+++ 07_Nums_And_Maths.js
@@ -0,0 +1,36 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log( balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 1123.886
+
+// console.log(otherNumber.toPrecision(5))
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString())
+
+
+
+
+//++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.6))
+// console.log(Math.min(4,5,6,1,12))
+
+// console.log(Math.random())
+// console.log(Math.floor(Math.random()*10)+1)
+
+ const min = 10
+ const max = 20
+
+ console.log(Math.floor(Math.random()*(max-min +1))+min)
+
